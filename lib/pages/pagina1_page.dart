6bb6adc78c58e5dbc345028d05f2de8a6e24f92d
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-10-19 21:37:06 -0600

    Rama Cubit terminada

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
 
+import 'package:estados/bloc/usuario/usuario_cubit.dart';
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Pagina1Page extends StatelessWidget {
 
@@ -8,9 +11,14 @@ class Pagina1Page extends StatelessWidget {
   Widget build(BuildContext context) {
    return Scaffold(
       appBar: AppBar(
-           title: Text('Pagina1', textAlign: TextAlign.center,)
+           title: Text('Pagina1', textAlign: TextAlign.center),
+           actions: [
+             IconButton(icon: Icon(Icons.delete), onPressed: (){
+               context.bloc<UsuarioCubit>().borrarUsuario();
+             })
+           ],
       ),
-      body: InformacionUsuario(),
+      body: BodyScaffold(),
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.access_alarm),
         onPressed: ()  => Navigator.pushNamed(context, 'pagina2')
@@ -19,8 +27,31 @@ class Pagina1Page extends StatelessWidget {
   }
 }
 
-class InformacionUsuario extends StatelessWidget {
+class BodyScaffold extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UsuarioCubit, UsuarioState>(builder: (_, state) { 
+      
+      switch (state.runtimeType) {
+        case UsuarioInitial:
+            return Center(child: Text('No hay informacion del usuario'));
+          break;
+        case UsuarioActivo:
+          return InformacionUsuario( (state as UsuarioActivo).usuario );
+        break;
+        default:
+          return Center(child: Text('Estado no reconocido'));
+      }
+    });
+  }
+}
+
+class InformacionUsuario extends StatelessWidget {
+  final Usuario usuario;
+
+  const InformacionUsuario(this.usuario);
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,16 +64,15 @@ class InformacionUsuario extends StatelessWidget {
           Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
           
-          ListTile(title: Text('Nombre: ')),
-          ListTile(title: Text('Edad: ')),
+          ListTile(title: Text('Nombre: ${ usuario.nombre }')),
+          ListTile(title: Text('Edad: ${ usuario.edad }')),
           
           Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
           
-          ListTile(title: Text('Profesion 1: ')),
-          ListTile(title: Text('Profesion 2: ')),
-          ListTile(title: Text('Profesion 3: ')),
           
+          
+          ...usuario.profesiones.map((profesion) => ListTile(title: Text(profesion))).toList()          
         ],
       ),
     );
--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,11 +1,15 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:estados/bloc/usuario/usuario_cubit.dart';
+import 'package:estados/models/usuario.dart';
 
 class Pagina2Page extends StatelessWidget {
 
 @override
   Widget build(BuildContext context) {
+   final usuarioCubit = context.bloc<UsuarioCubit>();
    return Scaffold(
       appBar: AppBar(
            title: Text('Pagina2', textAlign: TextAlign.center,)
@@ -18,21 +22,30 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Establecer usuario', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
+                final newUser = new Usuario(
+                  nombre: 'Orlin Alvarado',
+                  edad: 34, 
+                  profesiones: [
+                    'FullStack Developer',
+                    'Database manager'
+                  ]
+                );
                 
+               usuarioCubit.selecccionarUsuario(newUser);
               }
             ),
             MaterialButton(
               child: Text('Cambiar edad', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-                
+                usuarioCubit.cambiarEdad(30);
               }
             ),
             MaterialButton(
               child: Text('Añadir profesiones', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-                
+                usuarioCubit.agregarProfesion();
               }
             ),
           ],
